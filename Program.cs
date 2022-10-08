38fe65798c50b3e50a927f4ccd374942600a2b6b
Alex <automex74@gmail.com>
2022-10-08 16:20:04 +0500

    add task_21

--- Program.cs
+++ Program.cs
@@ -18,3 +18,23 @@ else
 {
     Console.WriteLine("Введено не пятизначное число. ");
 }
+
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+Console.WriteLine("--------------Задача №21.--------------");
+Console.WriteLine("Введите координату x точки A: ");
+double xa = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату y точки A: ");
+double ya = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату z точки A: ");
+double za = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату x точки B: ");
+double xb = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату y точки B: ");
+double yb = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату z точки B: ");
+double zb = int.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+
+Console.WriteLine($"Расстояние между точками {result:f2}");
